0ccf23055ada1f3cba65e5f2698edf22b6bada11
pedr097 <pedro.paraguacu@gmail.com>
2019-05-28 20:41:31 -0300

    nova-materia-prima

---
+++ src/app/pages/nova-materia-prima/nova-materia-prima.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-materia-prima',
+  templateUrl: './nova-materia-prima.page.html',
+  styleUrls: ['./nova-materia-prima.page.scss'],
+})
+export class NovaMateriaPrimaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
